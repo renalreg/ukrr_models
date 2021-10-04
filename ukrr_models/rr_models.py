3719e8d5cd59a7568996da6fe1f6f03d2aecc440
George Swinnerton <george.swinnerton@renalregistry.nhs.uk>
2021-10-04 12:27:10 +0100

    Add export of Treatment records

--- ukrr_models/rr_models.py
+++ ukrr_models/rr_models.py
@@ -1,4 +1,3 @@
-from sqlalchemy.orm import relationship
 from sqlalchemy import (
     Boolean,
     Column,
@@ -10,6 +9,9 @@ from sqlalchemy import (
     String,
 )
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm.session import object_session
+
 
 import pyxb
 
@@ -29,14 +31,19 @@ Base = declarative_base()
 
 class UKRR_Patient(Base):
 
-    __tablename__ = 'patients'
+    __tablename__ = "patients"
+
+    # This is (probably) the last centre for which a file was
+    # loaded and not neccesarily the latest/main unit they
+    # attend.
+    hosp_centre = Column(String)
 
     rr_no = Column(Integer, primary_key=True)
     surname = Column(String)
     forename = Column(String)
     sex = Column(String)
 
-    nhs_no = Column('new_nhs_no', Integer)
+    nhs_no = Column("new_nhs_no", Integer)
     chi_no = Column(Integer)
     hsc_no = Column(Integer)
     uktssa_no = Column(Integer)
@@ -50,19 +57,21 @@ class UKRR_Patient(Base):
 
     first_seen_date = Column(Date)
 
-    patient_demographics = relationship("Patient_Demographics", backref="patient")
+    patient_demographics = relationship(
+        "Patient_Demographics", backref="patient", lazy="dynamic"
+    )
 
     def as_pyxb_xml(
         self,
         # If sending_facility is supplied we assume only
         # one set of data returned.
-        sending_facility: str = 'UKRR',
+        sending_facility: str = "UKRR",
         # TODO: UKRR is not a valid value for SendingExtract yet.
-        sending_extract: str = 'UKRDC',
-        full_patient_record: bool = True
+        sending_extract: str = "UKRDC",
+        full_patient_record: bool = True,
     ):
-    
-        if sending_facility == 'UKRR':
+
+        if sending_facility == "UKRR":
             full_patient_record = False
 
         patient_record = ukrdc_schema.PatientRecord()
@@ -78,7 +87,7 @@ class UKRR_Patient(Base):
 
         patient_record.Patient.PatientNumbers = pyxb.BIND()
 
-        if sending_facility == 'UKRR':
+        if sending_facility == "UKRR":
             patient_record.Patient.Names = pyxb.BIND(
                 pyxb.BIND(use="L", Family=self.surname, Given=self.forename)
             )
@@ -93,10 +102,20 @@ class UKRR_Patient(Base):
             patient_record.Patient.PatientNumbers.append(xml_identifier)
 
         else:
-            patient_demographics = self.patient_demographics.filter(hosp_centre==sending_facility).first()
+            patient_demographics = self.patient_demographics.filter(
+                Patient_Demographics.hosp_centre == sending_facility
+            ).first()
+
+            if patient_demographics:
+                surname = patient_demographics.surname
+                forename = patient_demographics.forename
+            else:
+                # TODO: This may be an issue with old records?
+                surname = self.surname
+                forename = self.forename
 
             patient_record.Patient.Names = pyxb.BIND(
-                pyxb.BIND(use="L", Family=patient_demographics.surname, Given=patient_demographics.forename)
+                pyxb.BIND(use="L", Family=surname, Given=forename)
             )
 
             for nhs_identifier in (self.nhs_no, self.chi_no, self.hsc_no):
@@ -121,7 +140,7 @@ class UKRR_Patient(Base):
 
                 xml_identifier.Number = str(nhs_identifier)
                 # TODO: Find the actual organization.
-                xml_identifier.Organization = 'NHS'
+                xml_identifier.Organization = "NHS"
                 xml_identifier.NumberType = "NI"
 
                 patient_record.Patient.PatientNumbers.append(xml_identifier)
@@ -131,25 +150,148 @@ class UKRR_Patient(Base):
 
         xml_identifier.Number = str(self.rr_no)
         # TODO: Find the actual organization.
-        xml_identifier.Organization = 'UKRR'
+        xml_identifier.Organization = "UKRR"
         xml_identifier.NumberType = "NI"
 
         patient_record.Patient.PatientNumbers.append(xml_identifier)
-        
-        if sending_facility == 'UKRR':
+
+        if sending_facility == "UKRR":
             # TODO: Create a Program Membership Here
             pass
 
         if full_patient_record:
             # TODO: Extract the other stuff here
-            pass
+
+            # Treatments
+
+            try:
+                # I suspect this can fail if you create an object
+                # outside of a session
+
+                sql_string = """
+                SELECT
+                    A.RR_NO,
+                    A.DATE_START AS FROM_TIME,
+                    A.TREATMENT_MODALITY AS ADMITREASONCODE,
+                    A.TREATMENT_CENTRE AS HEALTHCAREFACILITYCODE,
+                    B.DATE_START AS TO_TIME,
+                    B.TREATMENT_MODALITY AS DISCHARGEREASONCODE,
+                    B.TREATMENT_CENTRE AS DISCHARGELOCATION
+                FROM
+                (
+                SELECT
+                    RR_NO,
+                    DATE_START,
+                    DATE_END,
+                    YEAR_END_SEQ,
+                    TREATMENT_MODALITY,
+                    HOSP_CENTRE,
+                    TREATMENT_CENTRE,
+                    ROW_NUMBER() OVER (PARTITION BY RR_NO ORDER BY DATE_START ASC, YEAR_END_SEQ ASC) AS ROWNUMBER
+                FROM
+                    TREATMENT
+                ) A
+                LEFT JOIN
+                (
+                SELECT
+                    RR_NO,
+                    DATE_START,
+                    DATE_END,
+                    YEAR_END_SEQ,
+                    TREATMENT_MODALITY,
+                    HOSP_CENTRE,
+                    TREATMENT_CENTRE,
+                    ROW_NUMBER() OVER (PARTITION BY RR_NO ORDER BY DATE_START ASC, YEAR_END_SEQ ASC) AS ROWNUMBER
+                FROM
+                    TREATMENT
+                ) B
+                    ON
+                        A.RR_NO = B.RR_NO AND
+                        A.ROWNUMBER = B.ROWNUMBER - 1 AND
+                        A.HOSP_CENTRE = B.HOSP_CENTRE
+                LEFT JOIN MODALITY_CODES C ON C.REGISTRY_CODE = A.TREATMENT_MODALITY
+                LEFT JOIN MODALITY_CODES D ON D.REGISTRY_CODE = B.TREATMENT_MODALITY
+                WHERE
+                    A.RR_NO = :rr_no AND
+                    C.TRANSFER_OUT = 0 AND
+                    A.HOSP_CENTRE = :hosp_centre
+                ORDER BY
+                    A.RR_NO,
+                    A.DATE_START,
+                    A.YEAR_END_SEQ
+                """
+
+                cursor = (
+                    object_session(self)
+                    .execute(
+                        sql_string,
+                        {"rr_no": self.rr_no, "hosp_centre": sending_facility},
+                    )
+                    .cursor
+                )
+                results = cursor.fetchall()
+
+                if results:
+                    patient_record.Encounters = pyxb.BIND()
+
+                for row in results:
+
+                    from_time = row[1]
+                    admitreasoncode = row[2]
+                    healthcarefacilitycode = row[3]
+                    to_time = row[4]
+                    dischargereason = row[5]
+                    dischargelocation = row[6]
+
+                    treatment = ukrdc_schema.Treatment()
+
+                    treatment.FromTime = from_time
+                    treatment.ToTime = to_time
+
+                    if healthcarefacilitycode:
+                        treatment.HealthCareFacility = ukrdc_schema.Location()
+                        treatment.HealthCareFacility.Code = healthcarefacilitycode
+                        treatment.HealthCareFacility.CodingStandard = "ODS"
+
+                    if admitreasoncode:
+                        treatment.AdmitReason = ukrdc_schema.CF_RR7_TREATMENT()
+                        treatment.AdmitReason.Code = admitreasoncode
+                        treatment.AdmitReason.CodingStandard = "CF_RR7_TREATMENT"
+
+                    # TODO: This needs to be a lookup to MODALITY_CODES
+                    # Or, I wonder if you can get it out of the PyXB Model?
+                    if dischargereason in (
+                        "30",
+                        "38",
+                        "84",
+                        "85",
+                        "86",
+                        "90",
+                        "91",
+                        "92",
+                        "95",
+                    ):
+                        treatment.DischargeReason = ukrdc_schema.CF_RR7_DISCHARGE()
+                        treatment.DischargeReason.Code = dischargereason
+                        treatment.DischargeReason.CodingStandard = "CF_RR7_DISCHARGE"
+
+                        # Only output the location if we have a reason.
+                        if dischargelocation:
+                            treatment.DischargeLocation = ukrdc_schema.Location()
+                            treatment.DischargeLocation.Code = dischargelocation
+                            treatment.DischargeLocation.CodingStandard = "RR1+"
+
+                    patient_record.Encounters.append(treatment)
+
+            except Exception:
+                raise
 
         return patient_record.toDOM().toprettyxml()
 
 
 class Patient_Demographics(Base):
 
-    __tablename__ = 'patient_demog'
+    __tablename__ = "patient_demog"
 
     rr_no = Column(Integer, ForeignKey("patients.rr_no"), primary_key=True)
     hosp_centre = Column(String, primary_key=True)
@@ -161,7 +303,7 @@ class Patient_Demographics(Base):
     date_birth = Column(Date)
     date_death = Column(Date)
 
-    nhs_no = Column('new_nhs_no', Integer)
+    nhs_no = Column("new_nhs_no", Integer)
     chi_no = Column(Integer)
     hsc_no = Column(Integer)
     uktssa_no = Column(Integer)
@@ -178,7 +320,7 @@ class UKRR_Deleted_Patient(Base):
     forename = Column(String)
     sex = Column(String)
 
-    nhs_no = Column('new_nhs_no', Integer)
+    nhs_no = Column("new_nhs_no", Integer)
     chi_no = Column(Integer)
     hsc_no = Column(Integer)
     uktssa_no = Column(Integer)
