72b3fd2438cc7586c3bab2f5c6444c63aace7c65
phil <philip.main@renalregistry.nhs.uk>
2023-04-24 14:16:35 +0100

    removed xml utils into a seperate repository

--- setup.py
+++ setup.py
@@ -1,31 +1,8 @@
 from setuptools import setup
 import re
-
-test_deps = [
-    "coverage2clover==1.4.0",
-    "flake8",
-    "flake8-junit-report",
-    "mypy",
-    "pytest-cov>=2.10",
-    "pytest>=6.1",
-    "tox",
-    "types-setuptools",
-    "unittest-xml-reporting",
-]
-
-# requirements for the utility functions which produce 
-extract_requirements = [
-    "rr.nhs @ git+ssh://git@github.com/renalreg/rr-nhs.git",
-    "git+ssh://git@github.com/renalreg/cohort_extract.git#egg=ukrdc_cohort_extract",
-    "ukrdc.database @ git+ssh://git@github.com/renalreg/ukrdc_database.git",
-    "uuid",
-    "ukrdc_schema",
-    "pyxb"
-]
-
 setup(
     name="ukrr_models",
-    version="0.0.4",
+    version="1.0.0",
     author="UK Renal Registry",
     author_email="rrsystems@renalregistry.nhs.uk",
     url="https://www.renalreg.org/",
@@ -34,9 +11,4 @@ setup(
         "sqlalchemy"
     ],
     zip_safe=True,
-    tests_require=test_deps,
-    extras_require={
-        "test": test_deps,
-        "extract": extract_requirements
-    },
 )
