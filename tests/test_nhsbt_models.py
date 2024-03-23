60dd4ad4e877e89d723198171da9de106c39da2a
andyatterton <andrew.atterton@ukkidney.org>
2024-03-23 19:18:57 +0000

    feat(CICD): :sparkles: poetry and github support
    Added pyproject.toml for poetry. Added standard set of github actions. Updated tox to reflect the standard set of tools. Added simple tests. Added a changelog and readme. Reformatted and linted. Improved the gitignore

---
+++ tests/test_nhsbt_models.py
@@ -0,0 +1,30 @@
+import ast
+
+from sqlalchemy import create_mock_engine
+from sqlalchemy.sql.type_api import TypeEngine
+
+from ukrr_models.nhsbt_models import Base
+from tests.table_extractor import TableNameExtractor
+
+
+def mssql_dump(sql: TypeEngine, *args, **kwargs):
+    dialect = sql.compile(dialect=mssql_engine.dialect)
+    if sql_str := str(dialect).rstrip():
+        print(f"{sql_str};")
+
+
+mssql_engine = create_mock_engine("mssql+pyodbc://", mssql_dump)
+
+with open("./ukrr_models/nhsbt_models.py", "r") as file:
+    code = file.read()
+
+tree = ast.parse(code)
+table_name_extractor = TableNameExtractor()
+table_name_extractor.visit(tree)
+
+
+def test_create_tables(capsys):
+    Base.metadata.create_all(bind=mssql_engine, checkfirst=False)
+    captured = capsys.readouterr()
+    for table in table_name_extractor.table_names:
+        assert f"CREATE TABLE {table}" in captured.out
