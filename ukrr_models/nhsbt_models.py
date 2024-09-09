6e0f70b4236c61f92618002347c91ff99c08e309
andyatterton <andrew.atterton@ukkidney.org>
2024-09-09 13:02:35 +0100

    mypy fix

--- ukrr_models/nhsbt_models.py
+++ ukrr_models/nhsbt_models.py
@@ -11,114 +11,114 @@ class UKTPatient(Base):
     __tablename__ = "UKT_PATIENTS"
 
     UKTSSA_NO = Column(Integer, primary_key=True, autoincrement=False)
-    uktssa_no = synonym("UKTSSA_NO")
+    uktssa_no: Column = synonym("UKTSSA_NO")
 
     SURNAME = Column(String(50))
-    surname = synonym("SURNAME")
+    surname: Column = synonym("SURNAME")
 
     FORENAME = Column(String(50))
-    forename = synonym("FORENAME")
+    forename: Column = synonym("FORENAME")
 
     SEX = Column(String(1))
-    sex = synonym("SEX")
+    sex: Column = synonym("SEX")
 
     POST_CODE = Column(String(10))
-    post_code = synonym("POST_CODE")
+    post_code: Column = synonym("POST_CODE")
 
     NEW_NHS_NO = Column(Integer)
-    new_nhs_no = synonym("NEW_NHS_NO")
+    new_nhs_no: Column = synonym("NEW_NHS_NO")
 
     CHI_NO = Column(Integer)
-    chi_no = synonym("CHI_NO")
+    chi_no: Column = synonym("CHI_NO")
 
     HSC_NO = Column(Integer)
-    hsc_no = synonym("HSC_NO")
+    hsc_no: Column = synonym("HSC_NO")
 
     RR_NO = Column(Integer)
-    rr_no = synonym("RR_NO")
+    rr_no: Column = synonym("RR_NO")
 
     UKT_DATE_DEATH = Column(DateTime)
-    ukt_date_death = synonym("UKT_DATE_DEATH")
+    ukt_date_death: Column = synonym("UKT_DATE_DEATH")
 
     UKT_DATE_BIRTH = Column(DateTime)
-    ukt_date_birth = synonym("UKT_DATE_BIRTH")
+    ukt_date_birth: Column = synonym("UKT_DATE_BIRTH")
 
 
 class UKTTransplant(Base):
     __tablename__ = "UKT_TRANSPLANTS"
 
     REGISTRATION_ID = Column(String(12), primary_key=True)
-    registration_id = synonym("REGISTRATION_ID")
+    registration_id: Column = synonym("REGISTRATION_ID")
 
     UKTSSA_NO = Column(Integer)
-    uktssa_no = synonym("UKTSSA_NO")
+    uktssa_no: Column = synonym("UKTSSA_NO")
 
     TRANSPLANT_ID = Column(Integer)
-    transplant_id = synonym("TRANSPLANT_ID")
+    transplant_id: Column = synonym("TRANSPLANT_ID")
 
     TRANSPLANT_TYPE = Column(String(10))
-    transplant_type = synonym("TRANSPLANT_TYPE")
+    transplant_type: Column = synonym("TRANSPLANT_TYPE")
 
     TRANSPLANT_ORGAN = Column(String(50))
-    transplant_organ = synonym("TRANSPLANT_ORGAN")
+    transplant_organ: Column = synonym("TRANSPLANT_ORGAN")
 
     TRANSPLANT_UNIT = Column(String(50))
-    transplant_unit = synonym("TRANSPLANT_UNIT")
+    transplant_unit: Column = synonym("TRANSPLANT_UNIT")
 
     RR_NO = Column(Integer)
-    rr_no = synonym("RR_NO")
+    rr_no: Column = synonym("RR_NO")
 
     TRANSPLANT_DATE = Column(DateTime)
-    transplant_date = synonym("TRANSPLANT_DATE")
+    transplant_date: Column = synonym("TRANSPLANT_DATE")
 
     UKT_FAIL_DATE = Column(DateTime)
-    ukt_fail_date = synonym("UKT_FAIL_DATE")
+    ukt_fail_date: Column = synonym("UKT_FAIL_DATE")
 
     REGISTRATION_DATE = Column(DateTime)
-    registration_date = synonym("REGISTRATION_DATE")
+    registration_date: Column = synonym("REGISTRATION_DATE")
 
     REGISTRATION_DATE_TYPE = Column(String(12))
-    registration_date_type = synonym("REGISTRATION_DATE_TYPE")
+    registration_date_type: Column = synonym("REGISTRATION_DATE_TYPE")
 
     REGISTRATION_END_DATE = Column(DateTime)
-    registration_end_date = synonym("REGISTRATION_END_DATE")
+    registration_end_date: Column = synonym("REGISTRATION_END_DATE")
 
     REGISTRATION_END_STATUS = Column(String(12))
-    registration_end_status = synonym("REGISTRATION_END_STATUS")
+    registration_end_status: Column = synonym("REGISTRATION_END_STATUS")
 
     TRANSPLANT_CONSIDERATION = Column(String(20))
-    transplant_consideration = synonym("TRANSPLANT_CONSIDERATION")
+    transplant_consideration: Column = synonym("TRANSPLANT_CONSIDERATION")
 
     TRANSPLANT_DIALYSIS = Column(String(12))
-    transplant_dialysis = synonym("TRANSPLANT_DIALYSIS")
+    transplant_dialysis: Column = synonym("TRANSPLANT_DIALYSIS")
 
     TRANSPLANT_RELATIONSHIP = Column(String(20))
-    transplant_relationship = synonym("TRANSPLANT_RELATIONSHIP")
+    transplant_relationship: Column = synonym("TRANSPLANT_RELATIONSHIP")
 
     TRANSPLANT_SEX = Column(String(12))
-    transplant_sex = synonym("TRANSPLANT_SEX")
+    transplant_sex: Column = synonym("TRANSPLANT_SEX")
 
     CAUSE_OF_FAILURE = Column(String(10))
-    cause_of_failure = synonym("CAUSE_OF_FAILURE")
+    cause_of_failure: Column = synonym("CAUSE_OF_FAILURE")
 
     CAUSE_OF_FAILURE_TEXT = Column(String(500))
-    cause_of_failure_text = synonym("CAUSE_OF_FAILURE_TEXT")
+    cause_of_failure_text: Column = synonym("CAUSE_OF_FAILURE_TEXT")
 
     CIT_MINS = Column(String(10))
-    cit_mins = synonym("CIT_MINS")
+    cit_mins: Column = synonym("CIT_MINS")
 
     HLA_MISMATCH = Column(String(10))
-    hla_mismatch = synonym("HLA_MISMATCH")
+    hla_mismatch: Column = synonym("HLA_MISMATCH")
 
     UKT_SUSPENSION = Column(Boolean)
-    ukt_suspension = synonym("UKT_SUSPENSION")
+    ukt_suspension: Column = synonym("UKT_SUSPENSION")
 
 
 class UKTSites(Base):
     __tablename__ = "UKT_SITES"
 
     SITE_NAME = Column(String(50), primary_key=True)
-    site_name = synonym("SITE_NAME")
+    site_name: Column = synonym("SITE_NAME")
 
     RR_CODE = Column(String(8))
-    rr_code = synonym("RR_CODE")
+    rr_code: Column = synonym("RR_CODE")
--- ukrr_models/rr_models.py
+++ ukrr_models/rr_models.py
@@ -9,72 +9,68 @@ Base = declarative_base()
 class UKRRPatient(Base):
     __tablename__ = "PATIENTS"
 
-    # This is (probably) the last centre for which a file was
-    # loaded and not necessarily the latest/main unit they
-    # attend.
     HOSP_CENTRE = Column(String)
-    hosp_centre = synonym("HOSP_CENTRE")
+    hosp_centre: Column = synonym("HOSP_CENTRE")
 
     RR_NO = Column(Integer, primary_key=True)
-    rr_no = synonym("RR_NO")
+    rr_no: Column = synonym("RR_NO")
 
     SURNAME = Column(String)
-    surname = synonym("SURNAME")
+    surname: Column = synonym("SURNAME")
 
     FORENAME = Column(String)
-    forename = synonym("FORENAME")
+    forename: Column = synonym("FORENAME")
 
     SEX = Column(String)
-    sex = synonym("SEX")
+    sex: Column = synonym("SEX")
 
     NHS_NO = Column("NEW_NHS_NO", Integer)
-    nhs_no = synonym("NHS_NO")
+    nhs_no: Column = synonym("NHS_NO")
 
     CHI_NO = Column(Integer)
-    chi_no = synonym("CHI_NO")
+    chi_no: Column = synonym("CHI_NO")
 
     HSC_NO = Column(Integer)
-    hsc_no = synonym("HSC_NO")
+    hsc_no: Column = synonym("HSC_NO")
 
     UKTSSA_NO = Column(Integer)
-    uk_tssa_no = synonym("UKTSSA_NO")
+    uk_tssa_no: Column = synonym("UKTSSA_NO")
 
     DATE_BIRTH = Column(Date)
-    date_birth = synonym("DATE_BIRTH")
+    date_birth: Column = synonym("DATE_BIRTH")
 
     DATE_DEATH = Column(Date)
-    date_death = synonym("DATE_DEATH")
+    date_death: Column = synonym("DATE_DEATH")
 
     ETHNICITY = Column("ETHGR_CODE", String)
-    ethnicity = synonym("ETHNICITY")
+    ethnicity: Column = synonym("ETHNICITY")
 
     BLOOD_GROUP = Column(String)
-    blood_group = synonym("BLOOD_GROUP")
+    blood_group: Column = synonym("BLOOD_GROUP")
 
     BLOOD_RHESUS = Column("BLOOD_GROUP_RHESUS", String)
-    blood_rhesus = synonym("BLOOD_RHESUS")
+    blood_rhesus: Column = synonym("BLOOD_RHESUS")
 
     COD_READ = Column(String)
-    cod_read = synonym("COD_READ")
+    cod_read: Column = synonym("COD_READ")
 
     COD_EDTA1 = Column(Integer)
-    cod_edta1 = synonym("COD_EDTA1")
+    cod_edta1: Column = synonym("COD_EDTA1")
 
     COD_EDTA2 = Column(Integer)
-    cod_edta2 = synonym("COD_EDTA2")
+    cod_edta2: Column = synonym("COD_EDTA2")
 
     COD_TEXT = Column(String)
-    cod_text = synonym("COD_TEXT")
+    cod_text: Column = synonym("COD_TEXT")
 
-    # First loaded into the UKRR database on this date
     DATE_REGISTERED = Column(Date)
-    date_registered = synonym("DATE_REGISTERED")
+    date_registered: Column = synonym("DATE_REGISTERED")
 
     FIRST_SEEN_DATE = Column(Date)
-    first_seen_date = synonym("FIRST_SEEN_DATE")
+    first_seen_date: Column = synonym("FIRST_SEEN_DATE")
 
     OPT_OUT_FLAG = Column(String)
-    opt_out_flag = synonym("OPT_OUT_FLAG")
+    opt_out_flag: Column = synonym("OPT_OUT_FLAG")
 
     PATIENT_DEMOGRAPHICS: Mapped[List["Patient_Demographics"]] = relationship(
         back_populates="UKRR_PATIENT"
@@ -85,46 +81,46 @@ class Patient_Demographics(Base):
     __tablename__ = "PATIENT_DEMOG"
 
     RR_NO = Column(Integer, ForeignKey("PATIENTS.RR_NO"), primary_key=True)
-    rr_no = synonym("RR_NO")
+    rr_no: Column = synonym("RR_NO")
 
     HOSP_CENTRE = Column(String, primary_key=True)
-    hosp_centre = synonym("HOSP_CENTRE")
+    hosp_centre: Column = synonym("HOSP_CENTRE")
 
     SURNAME = Column(String)
-    surname = synonym("SURNAME")
+    surname: Column = synonym("SURNAME")
 
     FORENAME = Column(String)
-    forename = synonym("FORENAME")
+    forename: Column = synonym("FORENAME")
 
     BIRTH_NAME = Column(String)
-    birth_name = synonym("BIRTH_NAME")
+    birth_name: Column = synonym("BIRTH_NAME")
 
     ALIAS_NAME = Column(String)
-    alias_name = synonym("ALIAS_NAME")
+    alias_name: Column = synonym("ALIAS_NAME")
 
     DATE_BIRTH = Column(Date)
-    date_birth = synonym("DATE_BIRTH")
+    date_birth: Column = synonym("DATE_BIRTH")
 
     DATE_DEATH = Column(Date)
-    date_death = synonym("DATE_DEATH")
+    date_death: Column = synonym("DATE_DEATH")
 
     NHS_NO = Column("NEW_NHS_NO", Integer)
-    nhs_no = synonym("NHS_NO")
+    nhs_no: Column = synonym("NHS_NO")
 
     CHI_NO = Column(Integer)
-    chi_no = synonym("CHI_NO")
+    chi_no: Column = synonym("CHI_NO")
 
     HSC_NO = Column(Integer)
-    hsc_no = synonym("HSC_NO")
+    hsc_no: Column = synonym("HSC_NO")
 
     UKTSSA_NO = Column(Integer)
-    uk_tssa_no = synonym("UKTSSA_NO")
+    uk_tssa_no: Column = synonym("UKTSSA_NO")
 
     LOCAL_HOSP_NO = Column(String)
-    local_hosp_no = synonym("LOCAL_HOSP_NO")
+    local_hosp_no: Column = synonym("LOCAL_HOSP_NO")
 
     FIRST_SEEN_DATE = Column(Date)
-    first_seen_date = synonym("FIRST_SEEN_DATE")
+    first_seen_date: Column = synonym("FIRST_SEEN_DATE")
 
     UKRR_PATIENT: Mapped["UKRRPatient"] = relationship(
         back_populates="PATIENT_DEMOGRAPHICS"
@@ -135,239 +131,239 @@ class UKRR_Deleted_Patient(Base):
     __tablename__ = "DELETED_PATIENTS"
 
     RR_NO = Column(Integer, primary_key=True)
-    rr_no = synonym("RR_NO")
+    rr_no: Column = synonym("RR_NO")
 
     SURNAME = Column(String)
-    surname = synonym("SURNAME")
+    surname: Column = synonym("SURNAME")
 
     FORENAME = Column(String)
-    forename = synonym("FORENAME")
+    forename: Column = synonym("FORENAME")
 
     SEX = Column(String)
-    sex = synonym("SEX")
+    sex: Column = synonym("SEX")
 
     NHS_NO = Column("NEW_NHS_NO", Integer)
-    nhs_no = synonym("NHS_NO")
+    nhs_no: Column = synonym("NHS_NO")
 
     CHI_NO = Column(Integer)
-    chi_no = synonym("CHI_NO")
+    chi_no: Column = synonym("CHI_NO")
 
     HSC_NO = Column(Integer)
-    hsc_no = synonym("HSC_NO")
+    hsc_no: Column = synonym("HSC_NO")
 
     UKTSSA_NO = Column(Integer)
-    uk_tssa_no = synonym("UKTSSA_NO")
+    uk_tssa_no: Column = synonym("UKTSSA_NO")
 
     LOCAL_HOSP_NO = Column(String)
-    local_hosp_no = synonym("LOCAL_HOSP_NO")
+    local_hosp_no: Column = synonym("LOCAL_HOSP_NO")
 
     DATE_BIRTH = Column(Date)
-    date_birth = synonym("DATE_BIRTH")
+    date_birth: Column = synonym("DATE_BIRTH")
 
     DATE_DEATH = Column(Date)
-    date_death = synonym("DATE_DEATH")
+    date_death: Column = synonym("DATE_DEATH")
 
 
 class QuarterlyTreatment(Base):
     __tablename__ = "QUARTERLY_TREATMENT"
 
     RR_NO = Column(Integer, primary_key=True)
-    rr_no = synonym("RR_NO")
+    rr_no: Column = synonym("RR_NO")
 
     DATE_START = Column(DateTime, primary_key=True)
-    date_start = synonym("DATE_START")
+    date_start: Column = synonym("DATE_START")
 
     TREATMENT_MODALITY = Column(String, primary_key=True)
-    treatment_modality = synonym("TREATMENT_MODALITY")
+    treatment_modality: Column = synonym("TREATMENT_MODALITY")
 
     TREATMENT_CENTRE = Column(String, primary_key=True)
-    treatment_centre = synonym("TREATMENT_CENTRE")
+    treatment_centre: Column = synonym("TREATMENT_CENTRE")
 
     HOSP_CENTRE = Column(String, primary_key=True)
-    hosp_centre = synonym("HOSP_CENTRE")
+    hosp_centre: Column = synonym("HOSP_CENTRE")
 
     DATE_END = Column(DateTime)
-    date_end = synonym("DATE_END")
+    date_end: Column = synonym("DATE_END")
 
     ADD_HAEMO_ON_PD = Column(String)
-    add_haemo_on_pd = synonym("ADD_HAEMO_ON_PD")
+    add_haemo_on_pd: Column = synonym("ADD_HAEMO_ON_PD")
 
     CREATININE = Column(Numeric(38, 4))
-    creatinine = synonym("CREATININE")
+    creatinine: Column = synonym("CREATININE")
 
     UREA = Column(Numeric(38, 4))
-    urea = synonym("UREA")
+    urea: Column = synonym("UREA")
 
     HAEMOGLOBIN = Column(Numeric(38, 4))
-    haemoglobin = synonym("HAEMOGLOBIN")
+    haemoglobin: Column = synonym("HAEMOGLOBIN")
 
     FERRETIN = Column(Numeric(38, 4))
-    ferretin = synonym("FERRETIN")
+    ferretin: Column = synonym("FERRETIN")
 
     ALBUMIN = Column(Numeric(38, 4))
-    albumin = synonym("ALBUMIN")
+    albumin: Column = synonym("ALBUMIN")
 
     ALUMINIUM = Column(Numeric(38, 4))
-    aluminium = synonym("ALUMINIUM")
+    aluminium: Column = synonym("ALUMINIUM")
 
     HBA1C = Column(Numeric(38, 4))
-    hba1c = synonym("HBA1C")
+    hba1c: Column = synonym("HBA1C")
 
     CHOLESTEROL = Column(Numeric(38, 4))
-    cholesterol = synonym("CHOLESTEROL")
+    cholesterol: Column = synonym("CHOLESTEROL")
 
     IPTH = Column(Numeric(38, 4))
-    ipth = synonym("IPTH")
+    ipth: Column = synonym("IPTH")
 
     CALCIUM_UNCORR = Column(Numeric(38, 4))
-    calcium_uncorr = synonym("CALCIUM_UNCORR")
+    calcium_uncorr: Column = synonym("CALCIUM_UNCORR")
 
     CALCIUM_CORR = Column(Numeric(38, 4))
-    calcium_corr = synonym("CALCIUM_CORR")
+    calcium_corr: Column = synonym("CALCIUM_CORR")
 
     PHOSPHATE = Column(Numeric(38, 4))
-    phosphate = synonym("PHOSPHATE")
+    phosphate: Column = synonym("PHOSPHATE")
 
     BICARBONATE = Column(Numeric(38, 4))
-    bicarbonate = synonym("BICARBONATE")
+    bicarbonate: Column = synonym("BICARBONATE")
 
     SYSTOLIC_BP = Column(Numeric(20, 0))
-    systolic_bp = synonym("SYSTOLIC_BP")
+    systolic_bp: Column = synonym("SYSTOLIC_BP")
 
     DIASTOLIC_BP = Column(Numeric(20, 0))
-    diastolic_bp = synonym("DIASTOLIC_BP")
+    diastolic_bp: Column = synonym("DIASTOLIC_BP")
 
     WEIGHT = Column(Numeric(38, 4))
-    weight = synonym("WEIGHT")
+    weight: Column = synonym("WEIGHT")
 
     UREA_REDUCTION_RATIO = Column(Numeric(38, 4))
-    urea_reduction_ratio = synonym("UREA_REDUCTION_RATIO")
+    urea_reduction_ratio: Column = synonym("UREA_REDUCTION_RATIO")
 
     EPO_USE = Column(String)
-    epo_use = synonym("EPO_USE")
+    epo_use: Column = synonym("EPO_USE")
 
     HD_SUPERVISION = Column(String)
-    hd_supervision = synonym("HD_SUPERVISION")
+    hd_supervision: Column = synonym("HD_SUPERVISION")
 
     DIALYSER_USED = Column(String)
-    dialyser_used = synonym("DIALYSER_USED")
+    dialyser_used: Column = synonym("DIALYSER_USED")
 
     FLOW_RATE = Column(Numeric(20, 0))
-    flow_rate = synonym("FLOW_RATE")
+    flow_rate: Column = synonym("FLOW_RATE")
 
     DIAL_REUSE = Column(String)
-    dial_reuse = synonym("DIAL_REUSE")
+    dial_reuse: Column = synonym("DIAL_REUSE")
 
     TIMES_PER_WEEK = Column(Numeric(20, 0))
-    times_per_week = synonym("TIMES_PER_WEEK")
+    times_per_week: Column = synonym("TIMES_PER_WEEK")
 
     DIAL_TIME = Column(Numeric(38, 4))
-    dial_time = synonym("DIAL_TIME")
+    dial_time: Column = synonym("DIAL_TIME")
 
     BICARB_DIAL = Column(String)
-    bicarb_dial = synonym("BICARB_DIAL")
+    bicarb_dial: Column = synonym("BICARB_DIAL")
 
     WEEKLY_FLUID_VOL = Column(Numeric(38, 4))
-    weekly_fluid_vol = synonym("WEEKLY_FLUID_VOL")
+    weekly_fluid_vol: Column = synonym("WEEKLY_FLUID_VOL")
 
     BAG_SIZE = Column(Numeric(38, 4))
-    bag_size = synonym("BAG_SIZE")
+    bag_size: Column = synonym("BAG_SIZE")
 
     CENTRE_PRI = Column(String)
-    centre_pri = synonym("CENTRE_PRI")
+    centre_pri: Column = synonym("CENTRE_PRI")
 
     POST_SYSTOLIC_BP = Column(Numeric(20, 0))
-    post_systolic_bp = synonym("POST_SYSTOLIC_BP")
+    post_systolic_bp: Column = synonym("POST_SYSTOLIC_BP")
 
     POST_DIASTOLIC_BP = Column(Numeric(20, 0))
-    post_diastolic_bp = synonym("POST_DIASTOLIC_BP")
+    post_diastolic_bp: Column = synonym("POST_DIASTOLIC_BP")
 
     SODIUM = Column(Numeric(38, 4))
-    sodium = synonym("SODIUM")
+    sodium: Column = synonym("SODIUM")
 
     PD_DIALYSATE_KTV = Column(Numeric(38, 4))
-    pd_dialysate_ktv = synonym("PD_DIALYSATE_KTV")
+    pd_dialysate_ktv: Column = synonym("PD_DIALYSATE_KTV")
 
     PD_URINE_KTV = Column(Numeric(38, 4))
-    pd_urine_ktv = synonym("PD_URINE_KTV")
+    pd_urine_ktv: Column = synonym("PD_URINE_KTV")
 
     PD_NPCR = Column(Numeric(38, 4))
-    pd_npcr = synonym("PD_NPCR")
+    pd_npcr: Column = synonym("PD_NPCR")
 
     CRP = Column(Numeric(38, 4))
-    crp = synonym("CRP")
+    crp: Column = synonym("CRP")
 
     LDL_CHOLESTEROL = Column(Numeric(38, 4))
-    ldl_cholesterol = synonym("LDL_CHOLESTEROL")
+    ldl_cholesterol: Column = synonym("LDL_CHOLESTEROL")
 
     HDL_CHOLESTEROL = Column(Numeric(38, 4))
-    hdl_cholesterol = synonym("HDL_CHOLESTEROL")
+    hdl_cholesterol: Column = synonym("HDL_CHOLESTEROL")
 
     TRIGLYCERIDES = Column(Numeric(38, 4))
-    triglycerides = synonym("TRIGLYCERIDES")
+    triglycerides: Column = synonym("TRIGLYCERIDES")
 
     WAITING_LIST_STATUS = Column(String)
-    waiting_list_status = synonym("WAITING_LIST_STATUS")
+    waiting_list_status: Column = synonym("WAITING_LIST_STATUS")
 
     CREATININE_FIRST_MONTH = Column(Numeric(38, 4))
-    creatinine_first_month = synonym("CREATININE_FIRST_MONTH")
+    creatinine_first_month: Column = synonym("CREATININE_FIRST_MONTH")
 
     CREATININE_SECOND_MONTH = Column(Numeric(38, 4))
-    creatinine_second_month = synonym("CREATININE_SECOND_MONTH")
+    creatinine_second_month: Column = synonym("CREATININE_SECOND_MONTH")
 
     PERCENT_HYPOCHROMIC = Column(Numeric(38, 4))
-    percent_hypochromic = synonym("PERCENT_HYPOCHROMIC")
+    percent_hypochromic: Column = synonym("PERCENT_HYPOCHROMIC")
 
     MCH = Column(Numeric(38, 4))
-    mch = synonym("MCH")
+    mch: Column = synonym("MCH")
 
     B12 = Column(Numeric(38, 4))
-    b12 = synonym("B12")
+    b12: Column = synonym("B12")
 
     RED_CELL_FOLATE = Column(Numeric(38, 4))
-    red_cell_folate = synonym("RED_CELL_FOLATE")
+    red_cell_folate: Column = synonym("RED_CELL_FOLATE")
 
     TRANSFERRIN_SATURATION = Column(Numeric(38, 4))
-    transferrin_saturation = synonym("TRANSFERRIN_SATURATION")
+    transferrin_saturation: Column = synonym("TRANSFERRIN_SATURATION")
 
     SERUM_POTASSIUM = Column(Numeric(38, 4))
-    serum_potassium = synonym("SERUM_POTASSIUM")
+    serum_potassium: Column = synonym("SERUM_POTASSIUM")
 
     PROTEIN_CREATININE_RATIO = Column(Numeric(38, 4))
-    protein_creatinine_ratio = synonym("PROTEIN_CREATININE_RATIO")
+    protein_creatinine_ratio: Column = synonym("PROTEIN_CREATININE_RATIO")
 
     ALBUMIN_CREATININE_RATIO = Column(Numeric(38, 4))
-    albumin_creatinine_ratio = synonym("ALBUMIN_CREATININE_RATIO")
+    albumin_creatinine_ratio: Column = synonym("ALBUMIN_CREATININE_RATIO")
 
     SERUM_CELL_FOLATE = Column(Numeric(38, 4))
-    serum_cell_folate = synonym("SERUM_CELL_FOLATE")
+    serum_cell_folate: Column = synonym("SERUM_CELL_FOLATE")
 
     ACE_INHIBITOR = Column(String)
-    ace_inhibitor = synonym("ACE_INHIBITOR")
+    ace_inhibitor: Column = synonym("ACE_INHIBITOR")
 
     RENAGEL = Column(String)
-    renagel = synonym("RENAGEL")
+    renagel: Column = synonym("RENAGEL")
 
     LANTHANUM = Column(String)
-    lanthanum = synonym("LANTHANUM")
+    lanthanum: Column = synonym("LANTHANUM")
 
     CINACALCET = Column(String)
-    cinacalcet = synonym("CINACALCET")
+    cinacalcet: Column = synonym("CINACALCET")
 
     CALCIUM_BASED_BINDER = Column(String)
-    calcium_based_binder = synonym("CALCIUM_BASED_BINDER")
+    calcium_based_binder: Column = synonym("CALCIUM_BASED_BINDER")
 
     ALUCAPS = Column(String)
-    alucaps = synonym("ALUCAPS")
+    alucaps: Column = synonym("ALUCAPS")
 
     SERUM_URATE = Column(Numeric(38, 4))
-    serum_urate = synonym("SERUM_URATE")
+    serum_urate: Column = synonym("SERUM_URATE")
 
     STATIN_DRUG_USE = Column(String)
-    statin_drug_use = synonym("STATIN_DRUG_USE")
+    statin_drug_use: Column = synonym("STATIN_DRUG_USE")
 
     HBA1C_MMOL = Column(Numeric(3, 0))
-    hba1c_mmol = synonym("HBA1C_MMOL")
+    hba1c_mmol: Column = synonym("HBA1C_MMOL")
 
     ALKALINE_PHOSPHATASE = Column(Numeric(38, 4))
-    alkaline_phosphatase = synonym("ALKALINE_PHOSPHATASE")
+    alkaline_phosphatase: Column = synonym("ALKALINE_PHOSPHATASE")
