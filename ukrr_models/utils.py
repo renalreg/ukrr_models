6d5391f6b91bfa23f9d205f681842e5f881fc14d
GlebRysev-UKKA <gleb.rysev@ukkidney.org>
2026-03-13 14:45:26 +0000

    fix: column renames and proper time formats

--- ukrr_models/utils.py
+++ ukrr_models/utils.py
@@ -3,8 +3,8 @@ from typing import Optional
 
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-from sqlalchemy import func, cast, Date
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import func, cast, Date, text
 from rr_database.sqlserver import SQLServerDatabase
 
 from ukrr_models.rr_models import UKRRPatient, UKRR_Deleted_Patient
@@ -15,9 +15,12 @@ def audit_date_expression():
 
 
 def audit_time_expression():
-    return func.concat(
-        func.datepart("hour", func.getdate()),
-        func.datepart("minute", func.getdate()),
+    ''' 
+    Return time in format 1945 (for 19:45) or 0705 (for 07:05)
+    '''
+    return text(
+        "RIGHT('0' + CAST(DATEPART(hour, GETDATE()) AS VARCHAR(2)), 2) + "
+        "RIGHT('0' + CAST(DATEPART(minute, GETDATE()) AS VARCHAR(2)), 2)"
     )
 
 
@@ -41,6 +44,11 @@ def delete_patient(
     if patient is None:
         raise DeletePatientError(f"Patient (RR_NO={rrno}) not found")
 
+    # Column is called NHS_NO in the model, but NEW_NHS_NO in the DB
+    rename_map = {
+        "NEW_NHS_NO": "NHS_NO",
+    }
+    
     # Find list of columns we need to copy from PATIENTS to DELETED_PATIENTS
     patient_columns = {c.key for c in UKRRPatient.__table__.columns}
     deleted_patient_columns = {c.key for c in UKRR_Deleted_Patient.__table__.columns}
@@ -49,7 +57,7 @@ def delete_patient(
     print("Adding %s to DELETED_PATIENTS..." % rrno)
 
     patinet_params: dict[str, object] = {
-        col: getattr(patient, col) for col in shared_cols
+        rename_map.get(col, col): getattr(patient, rename_map.get(col, col)) for col in shared_cols
     }
 
     patinet_params.update(
@@ -66,7 +74,7 @@ def delete_patient(
         for key, value in patinet_params.items()
         if key in deleted_patient_columns
     }
-
+        
     deleted_patient = UKRR_Deleted_Patient(**deleted_patient_params)
     deleted_patient.audit_date = audit_date_expression()
     deleted_patient.audit_time = audit_time_expression()
@@ -95,7 +103,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    database = SQLServerDatabase.connect(server="RR-SQL-Live", database="renalreg")
+    database = SQLServerDatabase.connect(server="RR-SQL-Test", database="renalreg")
+    table_desc = database.table_definitions()
     with database.session as session:
         print(
             f"Deleting patient {args.rrno}, authorised by {args.authorised_by} for reason {args.reason}"
