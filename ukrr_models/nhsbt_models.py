f88374d3644b55670bd294f6d66e3b1d21fc1700
OliveraReeves <158471173+OliveraReeves@users.noreply.github.com>
2026-04-27 13:44:28 +0100

    fix(#51): fixed type hints of models (#52)
    * fix(#51): fixed type hints of models

--- ukrr_models/nhsbt_models.py
+++ ukrr_models/nhsbt_models.py
@@ -1,4 +1,5 @@
 """SQLAlchemy models for NHSBT"""
+from typing import Optional
 
 from sqlalchemy import Boolean, DateTime, Integer, MetaData, String
 from sqlalchemy.orm import Mapped, declarative_base, mapped_column, synonym
@@ -15,35 +16,35 @@ class UKTPatient(Base):
     )
     uktssa_no: Mapped[int] = synonym("UKTSSA_NO")
 
-    SURNAME: Mapped[str] = mapped_column(String(50))
-    surname: Mapped[str] = synonym("SURNAME")
+    SURNAME: Mapped[Optional[str]] = mapped_column(String(50))
+    surname: Mapped[Optional[str]] = synonym("SURNAME")
 
-    FORENAME: Mapped[str] = mapped_column(String(50))
-    forename: Mapped[str] = synonym("FORENAME")
+    FORENAME: Mapped[Optional[str]] = mapped_column(String(50))
+    forename: Mapped[Optional[str]] = synonym("FORENAME")
 
-    SEX: Mapped[str] = mapped_column(String(1))
-    sex: Mapped[str] = synonym("SEX")
+    SEX: Mapped[Optional[str]] = mapped_column(String(1))
+    sex: Mapped[Optional[str]] = synonym("SEX")
 
-    POST_CODE: Mapped[str] = mapped_column(String(10))
-    post_code: Mapped[str] = synonym("POST_CODE")
+    POST_CODE: Mapped[Optional[str]] = mapped_column(String(10))
+    post_code: Mapped[Optional[str]] = synonym("POST_CODE")
 
-    NEW_NHS_NO: Mapped[int] = mapped_column(Integer)
-    new_nhs_no: Mapped[int] = synonym("NEW_NHS_NO")
+    NEW_NHS_NO: Mapped[Optional[int]] = mapped_column(Integer)
+    new_nhs_no: Mapped[Optional[int]] = synonym("NEW_NHS_NO")
 
-    CHI_NO: Mapped[int] = mapped_column(Integer)
-    chi_no: Mapped[int] = synonym("CHI_NO")
+    CHI_NO: Mapped[Optional[int]] = mapped_column(Integer)
+    chi_no: Mapped[Optional[int]] = synonym("CHI_NO")
 
-    HSC_NO: Mapped[int] = mapped_column(Integer)
-    hsc_no: Mapped[int] = synonym("HSC_NO")
+    HSC_NO: Mapped[Optional[int]] = mapped_column(Integer)
+    hsc_no: Mapped[Optional[int]] = synonym("HSC_NO")
 
-    RR_NO: Mapped[int] = mapped_column(Integer)
-    rr_no: Mapped[int] = synonym("RR_NO")
+    RR_NO: Mapped[Optional[int]] = mapped_column(Integer)
+    rr_no: Mapped[Optional[int]] = synonym("RR_NO")
 
-    UKT_DATE_DEATH: Mapped[DateTime] = mapped_column(DateTime)
-    ukt_date_death: Mapped[DateTime] = synonym("UKT_DATE_DEATH")
+    UKT_DATE_DEATH: Mapped[Optional[DateTime]] = mapped_column(DateTime)
+    ukt_date_death: Mapped[Optional[DateTime]] = synonym("UKT_DATE_DEATH")
 
-    UKT_DATE_BIRTH: Mapped[DateTime] = mapped_column(DateTime)
-    ukt_date_birth: Mapped[DateTime] = synonym("UKT_DATE_BIRTH")
+    UKT_DATE_BIRTH: Mapped[Optional[DateTime]] = mapped_column(DateTime)
+    ukt_date_birth: Mapped[Optional[DateTime]] = synonym("UKT_DATE_BIRTH")
 
 
 class UKTTransplant(Base):
@@ -55,65 +56,65 @@ class UKTTransplant(Base):
     UKTSSA_NO: Mapped[int] = mapped_column(Integer)
     uktssa_no: Mapped[int] = synonym("UKTSSA_NO")
 
-    TRANSPLANT_ID: Mapped[int] = mapped_column(Integer)
-    transplant_id: Mapped[int] = synonym("TRANSPLANT_ID")
+    TRANSPLANT_ID: Mapped[Optional[int]] = mapped_column(Integer)
+    transplant_id: Mapped[Optional[int]] = synonym("TRANSPLANT_ID")
 
-    TRANSPLANT_TYPE: Mapped[str] = mapped_column(String(10))
-    transplant_type: Mapped[str] = synonym("TRANSPLANT_TYPE")
+    TRANSPLANT_TYPE: Mapped[Optional[str]] = mapped_column(String(10))
+    transplant_type: Mapped[Optional[str]] = synonym("TRANSPLANT_TYPE")
 
-    TRANSPLANT_ORGAN: Mapped[str] = mapped_column(String(50))
-    transplant_organ: Mapped[str] = synonym("TRANSPLANT_ORGAN")
+    TRANSPLANT_ORGAN: Mapped[Optional[str]] = mapped_column(String(50))
+    transplant_organ: Mapped[Optional[str]] = synonym("TRANSPLANT_ORGAN")
 
-    TRANSPLANT_UNIT: Mapped[str] = mapped_column(String(50))
-    transplant_unit: Mapped[str] = synonym("TRANSPLANT_UNIT")
+    TRANSPLANT_UNIT: Mapped[Optional[str]] = mapped_column(String(50))
+    transplant_unit: Mapped[Optional[str]] = synonym("TRANSPLANT_UNIT")
 
-    RR_NO: Mapped[int] = mapped_column(Integer)
-    rr_no: Mapped[int] = synonym("RR_NO")
+    RR_NO: Mapped[Optional[int]] = mapped_column(Integer)
+    rr_no: Mapped[Optional[int]] = synonym("RR_NO")
 
-    TRANSPLANT_DATE: Mapped[DateTime] = mapped_column(DateTime)
-    transplant_date: Mapped[DateTime] = synonym("TRANSPLANT_DATE")
+    TRANSPLANT_DATE: Mapped[Optional[DateTime]] = mapped_column(DateTime)
+    transplant_date: Mapped[Optional[DateTime]] = synonym("TRANSPLANT_DATE")
 
-    UKT_FAIL_DATE: Mapped[DateTime] = mapped_column(DateTime)
-    ukt_fail_date: Mapped[DateTime] = synonym("UKT_FAIL_DATE")
+    UKT_FAIL_DATE: Mapped[Optional[DateTime]] = mapped_column(DateTime)
+    ukt_fail_date: Mapped[Optional[DateTime]] = synonym("UKT_FAIL_DATE")
 
-    REGISTRATION_DATE: Mapped[DateTime] = mapped_column(DateTime)
-    registration_date: Mapped[DateTime] = synonym("REGISTRATION_DATE")
+    REGISTRATION_DATE: Mapped[Optional[DateTime]] = mapped_column(DateTime)
+    registration_date: Mapped[Optional[DateTime]] = synonym("REGISTRATION_DATE")
 
-    REGISTRATION_DATE_TYPE: Mapped[str] = mapped_column(String(12))
-    registration_date_type: Mapped[str] = synonym("REGISTRATION_DATE_TYPE")
+    REGISTRATION_DATE_TYPE: Mapped[Optional[str]] = mapped_column(String(12))
+    registration_date_type: Mapped[Optional[str]] = synonym("REGISTRATION_DATE_TYPE")
 
-    REGISTRATION_END_DATE: Mapped[DateTime] = mapped_column(DateTime)
-    registration_end_date: Mapped[DateTime] = synonym("REGISTRATION_END_DATE")
+    REGISTRATION_END_DATE: Mapped[Optional[DateTime]] = mapped_column(DateTime)
+    registration_end_date: Mapped[Optional[DateTime]] = synonym("REGISTRATION_END_DATE")
 
-    REGISTRATION_END_STATUS: Mapped[str] = mapped_column(String(12))
-    registration_end_status: Mapped[str] = synonym("REGISTRATION_END_STATUS")
+    REGISTRATION_END_STATUS: Mapped[Optional[str]] = mapped_column(String(12))
+    registration_end_status: Mapped[Optional[str]] = synonym("REGISTRATION_END_STATUS")
 
-    TRANSPLANT_CONSIDERATION: Mapped[str] = mapped_column(String(20))
-    transplant_consideration: Mapped[str] = synonym("TRANSPLANT_CONSIDERATION")
+    TRANSPLANT_CONSIDERATION: Mapped[Optional[str]] = mapped_column(String(20))
+    transplant_consideration: Mapped[Optional[str]] = synonym("TRANSPLANT_CONSIDERATION")
 
-    TRANSPLANT_DIALYSIS: Mapped[str] = mapped_column(String(12))
-    transplant_dialysis: Mapped[str] = synonym("TRANSPLANT_DIALYSIS")
+    TRANSPLANT_DIALYSIS: Mapped[Optional[str]] = mapped_column(String(12))
+    transplant_dialysis: Mapped[Optional[str]] = synonym("TRANSPLANT_DIALYSIS")
 
-    TRANSPLANT_RELATIONSHIP: Mapped[str] = mapped_column(String(20))
-    transplant_relationship: Mapped[str] = synonym("TRANSPLANT_RELATIONSHIP")
+    TRANSPLANT_RELATIONSHIP: Mapped[Optional[str]] = mapped_column(String(20))
+    transplant_relationship: Mapped[Optional[str]] = synonym("TRANSPLANT_RELATIONSHIP")
 
-    TRANSPLANT_SEX: Mapped[str] = mapped_column(String(12))
-    transplant_sex: Mapped[str] = synonym("TRANSPLANT_SEX")
+    TRANSPLANT_SEX: Mapped[Optional[str]] = mapped_column(String(12))
+    transplant_sex: Mapped[Optional[str]] = synonym("TRANSPLANT_SEX")
 
-    CAUSE_OF_FAILURE: Mapped[str] = mapped_column(String(10))
-    cause_of_failure: Mapped[str] = synonym("CAUSE_OF_FAILURE")
+    CAUSE_OF_FAILURE: Mapped[Optional[str]] = mapped_column(String(10))
+    cause_of_failure: Mapped[Optional[str]] = synonym("CAUSE_OF_FAILURE")
 
-    CAUSE_OF_FAILURE_TEXT: Mapped[str] = mapped_column(String(500))
-    cause_of_failure_text: Mapped[str] = synonym("CAUSE_OF_FAILURE_TEXT")
+    CAUSE_OF_FAILURE_TEXT: Mapped[Optional[str]] = mapped_column(String(500))
+    cause_of_failure_text: Mapped[Optional[str]] = synonym("CAUSE_OF_FAILURE_TEXT")
 
-    CIT_MINS: Mapped[str] = mapped_column(String(10))
-    cit_mins: Mapped[str] = synonym("CIT_MINS")
+    CIT_MINS: Mapped[Optional[str]] = mapped_column(String(10))
+    cit_mins: Mapped[Optional[str]] = synonym("CIT_MINS")
 
-    HLA_MISMATCH: Mapped[str] = mapped_column(String(10))
-    hla_mismatch: Mapped[str] = synonym("HLA_MISMATCH")
+    HLA_MISMATCH: Mapped[Optional[str]] = mapped_column(String(10))
+    hla_mismatch: Mapped[Optional[str]] = synonym("HLA_MISMATCH")
 
-    UKT_SUSPENSION: Mapped[bool] = mapped_column(Boolean)
-    ukt_suspension: Mapped[bool] = synonym("UKT_SUSPENSION")
+    UKT_SUSPENSION: Mapped[Optional[bool]] = mapped_column(Boolean)
+    ukt_suspension: Mapped[Optional[bool]] = synonym("UKT_SUSPENSION")
 
 
 class UKTSites(Base):
